f15f18f379e9cff334cedc4b1aee8687929c1f63
Denis Bazhenov <bazhenov@farpost.com>
2017-08-31 16:18:22 +1000

    Initial version

---
+++ linear-counter.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class LinearCounter < Formula
+  desc "Linear counter C++ implementation"
+  homepage ""
+	url "https://github.com/bazhenov/linear-counter/archive/v0.1.tar.gz"
+  sha256 "a931dfba47bc1978ecf64b9659bdb2b0b8e03bcfb9f51599faa278c817ae100a"
+
+  depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test linear-counter`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
