4df24de2502e61e97ff9b9c81a2020fe688e2fa6
Denis Bazhenov <bazhenov@farpost.com>
2018-09-11 19:25:31 +1000

    Added bottle support for est package

--- est.rb
+++ est.rb
@@ -1,7 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Est < Formula
   desc "Fast probabilistic cli utilities for estimating cardinality"
   homepage "https://github.com/bazhenov/fast-estimate"
@@ -11,7 +7,7 @@ class Est < Formula
   depends_on "rust" => :build
 
 	bottle do
-		root_url "https://dl.bintray.com/dotsid/fast-estimate"
+		root_url "https://github.com/bazhenov/fast-estimate/releases/download/v0.1.1/"
     sha256 "06c66c00b63c1e2bb350ced0f5882b37012d7e068d7991522dbf124d7224c722" => :high_sierra
 	end
 
