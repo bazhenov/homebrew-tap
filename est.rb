2102de65d4b8a23eacd3b349e5bfe14b78c969a7
Denis Bazhenov <bazhenov@farpost.com>
2019-03-29 16:51:31 +1000

    Update version

--- est.rb
+++ est.rb
@@ -1,8 +1,8 @@
 class Est < Formula
   desc "Fast probabilistic cli utilities for estimating cardinality"
   homepage "https://github.com/bazhenov/fast-estimate"
-	url "https://github.com/bazhenov/fast-estimate/archive/v0.1.1.tar.gz"
-  sha256 "54a6b42186d470fdb7e43d2d26e3316690a6eb613a48ab22c305ce21674eabb5"
+	url "https://github.com/bazhenov/fast-estimate/archive/v0.2.tar.gz"
+  sha256 "2565bd92a3ce875ec483fafc0afaa83d3e599f5e50b25e9acf230e222c5d762f"
 
   depends_on "rust" => :build
 
