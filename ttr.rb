93dd8f0caa7ae2219ae5d6a4f1028686d8553895
Denis Bazhenov <dotsid@gmail.com>
2023-03-08 21:20:53 +0700

    ttr/0.2.0

--- ttr.rb
+++ ttr.rb
@@ -1,8 +1,8 @@
 class Ttr < Formula
   desc "Interactive terminal task runner"
   homepage "https://github.com/bazhenov/ttr"
-  url "https://github.com/bazhenov/ttr/releases/download/0.1.0/ttr-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "306e5f874942e725421499b2750e95050fb98fd94ef5b92fc00bbf85e458057f"
+  url "https://github.com/bazhenov/ttr/releases/download/0.2.0/ttr-0.2.0-x86_64-apple-darwin.tar.gz"
+  sha256 "8a1bf4ff6fe4832243170366109faa95c500087de0e631b12c1dcdfa1062e90a"
   license "MIT"
   head "https://github.com/bazhenov/ttr.git", branch: "master"
 
